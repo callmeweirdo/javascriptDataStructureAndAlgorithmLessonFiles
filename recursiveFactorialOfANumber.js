12d3f471662e1dd70f5b8ac4f95999cf8d2b9ecd
callmeweirdo <ogbandav102@gmail.com>
2023-10-20 01:32:27 +0100

    ive just learnt that the factorial of a non-negative integer 'n' is the product of all positive integer less than or equal to 'n'

---
+++ recursiveFactorialOfANumber.js
@@ -0,0 +1,12 @@
+//  ? Recursive Factorial of a number
+// ! Formular ---------------> n! = n * (n-1)!
+
+const recursiveFactorialOfANumber = (n) => {
+    if (n === 0) return 1 ;
+    const result = n * recursiveFactorialOfANumber(n - 1);
+    return result;
+}
+
+console.log(recursiveFactorialOfANumber(5));
+
+// ? te Big O notation --------> is the n = O(n) Linear 
--- recursiveFibonacciSequence.js
+++ recursiveFibonacciSequence.js
@@ -13,3 +13,20 @@ const recursiveFibunacci = (num) => {
 // recursiveFibunacci(6);
 
 console.log(recursiveFibunacci(20))
+
+
+
+
+// ?Fn = Fn - 1 + Fn - 2
+// ?F5 = F5 - 1 + F5 - 2
+// ?F5 = 4 + 3
+
+const recusiveFubunachiNumbers = (n) => {
+    if (n < 2) {
+        return n
+    };
+    const result = recusiveFubunachiNumbers(n - 1) + recusiveFubunachiNumbers(n - 2);
+    return result;
+}
+
+console.log(recusiveFubunachiNumbers(7));
